ed697eab1194082ea4a44538ffc0deacacd0db2a
Steve Lacey <steve@steve.ly>
2020-12-09 19:03:16 +0700

    Spec var name fixes

--- spec/job_spec.rb
+++ spec/job_spec.rb
@@ -6,132 +6,134 @@ describe CloudConvert::Job do
 
   describe "#==" do
     it "returns true when objects IDs are the same" do
-      user = CloudConvert::Job.new(id: id1, tasks: [{ operation: "convert" }])
+      job = CloudConvert::Job.new(id: id1, tasks: [{ operation: "convert" }])
       other = CloudConvert::Job.new(id: id1, tasks: [{ operation: "optimize" }])
-      expect(user == other).to be true
+      expect(job == other).to be true
     end
     it "returns false when objects IDs are different" do
-      user = CloudConvert::Job.new(id: id1)
+      job = CloudConvert::Job.new(id: id1)
       other = CloudConvert::Job.new(id: id2)
-      expect(user == other).to be false
+      expect(job == other).to be false
     end
     it "returns false when classes are different" do
-      user = CloudConvert::Job.new(id: id1)
+      job = CloudConvert::Job.new(id: id1)
       other = CloudConvert::Task.new(id: id1)
-      expect(user == other).to be false
+      expect(job == other).to be false
     end
   end
 
   describe "#tasks" do
     it "returns value when tasks is set" do
-      user = CloudConvert::Job.new(tasks: [{ operation: "convert" }])
-      expect(user.tasks).to eq [{ operation: "convert" }]
+      job = CloudConvert::Job.new(tasks: [{ operation: "convert" }])
+      expect(job.tasks.count).to eq 1
+      expect(job.tasks[0]).to be_a CloudConvert::Task
+      expect(job.tasks[0].operation).to eq "convert"
     end
-    it "returns nil when tasks is not set" do
-      user = CloudConvert::Job.new()
-      expect(user.tasks).to be_nil
+    it "returns [] when tasks is not set" do
+      job = CloudConvert::Job.new()
+      expect(job.tasks).to eq []
     end
   end
 
   describe "#status" do
     it "returns value when status is set" do
-      user = CloudConvert::Job.new(status: "pending")
-      expect(user.status).to be :pending
+      job = CloudConvert::Job.new(status: "pending")
+      expect(job.status).to be :pending
     end
     it "returns nil when status is not set" do
-      user = CloudConvert::Job.new()
-      expect(user.status).to be_nil
+      job = CloudConvert::Job.new()
+      expect(job.status).to be_nil
     end
   end
 
   describe "#tag" do
     it "returns value when tag is set" do
-      user = CloudConvert::Job.new(tag: "a tag")
-      expect(user.tag).to eq "a tag"
+      job = CloudConvert::Job.new(tag: "a tag")
+      expect(job.tag).to eq "a tag"
     end
     it "returns nil when tag is not set" do
-      user = CloudConvert::Job.new()
-      expect(user.tag).to be_nil
+      job = CloudConvert::Job.new()
+      expect(job.tag).to be_nil
     end
   end
 
   describe "#links" do
     it "returns struct when links is set" do
-      user = CloudConvert::Job.new(links: { self: url })
-      expect(user.links).to eq OpenStruct.new(self: url)
+      job = CloudConvert::Job.new(links: { self: url })
+      expect(job.links).to eq OpenStruct.new(self: url)
     end
     it "returns nil when links is not set" do
-      user = CloudConvert::Job.new()
-      expect(user.links).to be_nil
+      job = CloudConvert::Job.new()
+      expect(job.links).to be_nil
     end
   end
 
   describe "#created_at" do
     it "returns a Time when created_at is set" do
-      user = CloudConvert::Job.new(created_at: "Mon Jul 16 12:59:01 +0000 2020")
-      expect(user.created_at).to be_a Time
-      expect(user.created_at).to be_utc
+      job = CloudConvert::Job.new(created_at: "Mon Jul 16 12:59:01 +0000 2020")
+      expect(job.created_at).to be_a Time
+      expect(job.created_at).to be_utc
     end
     it "returns nil when created_at is not set" do
-      user = CloudConvert::Job.new()
-      expect(user.created_at).to be_nil
+      job = CloudConvert::Job.new()
+      expect(job.created_at).to be_nil
     end
   end
 
   describe "#created?" do
     it "returns true when created_at is set" do
-      user = CloudConvert::Job.new(created_at: "Mon Jul 16 12:59:01 +0000 2020")
-      expect(user.created?).to be true
+      job = CloudConvert::Job.new(created_at: "Mon Jul 16 12:59:01 +0000 2020")
+      expect(job.created?).to be true
     end
     it "returns false when created_at is not set" do
-      user = CloudConvert::Job.new()
-      expect(user.created?).to be false
+      job = CloudConvert::Job.new()
+      expect(job.created?).to be false
     end
   end
 
   describe "#started_at" do
     it "returns a Time when started_at is set" do
-      user = CloudConvert::Job.new(started_at: "Mon Jul 16 12:59:01 +0000 2020")
-      expect(user.started_at).to be_a Time
-      expect(user.started_at).to be_utc
+      job = CloudConvert::Job.new(started_at: "Mon Jul 16 12:59:01 +0000 2020")
+      expect(job.started_at).to be_a Time
+      expect(job.started_at).to be_utc
     end
     it "returns nil when started_at is not set" do
-      user = CloudConvert::Job.new()
-      expect(user.started_at).to be_nil
+      job = CloudConvert::Job.new()
+      expect(job.started_at).to be_nil
     end
   end
 
   describe "#started?" do
     it "returns true when started_at is set" do
-      user = CloudConvert::Job.new(started_at: "Mon Jul 16 12:59:01 +0000 2020")
-      expect(user.started?).to be true
+      job = CloudConvert::Job.new(started_at: "Mon Jul 16 12:59:01 +0000 2020")
+      expect(job.started?).to be true
     end
     it "returns false when started_at is not set" do
-      user = CloudConvert::Job.new()
-      expect(user.started?).to be false
+      job = CloudConvert::Job.new()
+      expect(job.started?).to be false
     end
   end
 
   describe "#ended_at" do
     it "returns a Time when ended_at is set" do
-      user = CloudConvert::Job.new(ended_at: "Mon Jul 16 12:59:01 +0000 2020")
-      expect(user.ended_at).to be_a Time
-      expect(user.ended_at).to be_utc
+      job = CloudConvert::Job.new(ended_at: "Mon Jul 16 12:59:01 +0000 2020")
+      expect(job.ended_at).to be_a Time
+      expect(job.ended_at).to be_utc
     end
     it "returns nil when ended_at is not set" do
-      user = CloudConvert::Job.new()
-      expect(user.ended_at).to be_nil
+      job = CloudConvert::Job.new()
+      expect(job.ended_at).to be_nil
     end
   end
 
   describe "#ended?" do
     it "returns true when ended_at is set" do
-      user = CloudConvert::Job.new(ended_at: "Mon Jul 16 12:59:01 +0000 2020")
-      expect(user.ended?).to be true
+      job = CloudConvert::Job.new(ended_at: "Mon Jul 16 12:59:01 +0000 2020")
+      expect(job.ended?).to be true
     end
     it "returns false when ended_at is not set" do
-      user = CloudConvert::Job.new()
-      expect(user.ended?).to be false
+      job = CloudConvert::Job.new()
+      expect(job.ended?).to be false
     end
   end
 end
--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -6,121 +6,121 @@ describe CloudConvert::Task do
 
   describe "#==" do
     it "returns true when objects IDs are the same" do
-      user = CloudConvert::Task.new(id: id1, operation: "convert")
+      task = CloudConvert::Task.new(id: id1, operation: "convert")
       other = CloudConvert::Task.new(id: id1, operation: "optimize")
-      expect(user == other).to be true
+      expect(task == other).to be true
     end
     it "returns false when objects IDs are different" do
-      user = CloudConvert::Task.new(id: id1)
+      task = CloudConvert::Task.new(id: id1)
       other = CloudConvert::Task.new(id: id2)
-      expect(user == other).to be false
+      expect(task == other).to be false
     end
     it "returns false when classes are different" do
-      user = CloudConvert::Task.new(id: id1)
+      task = CloudConvert::Task.new(id: id1)
       other = CloudConvert::Job.new(id: id1)
-      expect(user == other).to be false
+      expect(task == other).to be false
     end
   end
 
   describe "#operation" do
     it "returns value when operation is set" do
-      user = CloudConvert::Task.new(operation: "convert")
-      expect(user.operation).to be :convert
+      task = CloudConvert::Task.new(operation: "convert")
+      expect(task.operation).to eq "convert"
     end
     it "returns nil when operation is not set" do
-      user = CloudConvert::Task.new()
-      expect(user.operation).to be_nil
+      task = CloudConvert::Task.new()
+      expect(task.operation).to be_nil
     end
   end
 
   describe "#status" do
     it "returns value when status is set" do
-      user = CloudConvert::Task.new(status: "pending")
-      expect(user.status).to be :pending
+      task = CloudConvert::Task.new(status: "pending")
+      expect(task.status).to be :pending
     end
     it "returns nil when status is not set" do
-      user = CloudConvert::Task.new()
-      expect(user.status).to be_nil
+      task = CloudConvert::Task.new()
+      expect(task.status).to be_nil
     end
   end
 
   describe "#links" do
     it "returns struct when links is set" do
-      user = CloudConvert::Task.new(links: { self: url })
-      expect(user.links).to eq OpenStruct.new(self: url)
+      task = CloudConvert::Task.new(links: { self: url })
+      expect(task.links).to eq OpenStruct.new(self: url)
     end
     it "returns nil when links is not set" do
-      user = CloudConvert::Task.new()
-      expect(user.links).to be_nil
+      task = CloudConvert::Task.new()
+      expect(task.links).to be_nil
     end
   end
 
   describe "#created_at" do
     it "returns a Time when created_at is set" do
-      user = CloudConvert::Task.new(created_at: "Mon Jul 16 12:59:01 +0000 2020")
-      expect(user.created_at).to be_a Time
-      expect(user.created_at).to be_utc
+      task = CloudConvert::Task.new(created_at: "Mon Jul 16 12:59:01 +0000 2020")
+      expect(task.created_at).to be_a Time
+      expect(task.created_at).to be_utc
     end
     it "returns nil when created_at is not set" do
-      user = CloudConvert::Task.new()
-      expect(user.created_at).to be_nil
+      task = CloudConvert::Task.new()
+      expect(task.created_at).to be_nil
     end
   end
 
   describe "#created?" do
     it "returns true when created_at is set" do
-      user = CloudConvert::Task.new(created_at: "Mon Jul 16 12:59:01 +0000 2020")
-      expect(user.created?).to be true
+      task = CloudConvert::Task.new(created_at: "Mon Jul 16 12:59:01 +0000 2020")
+      expect(task.created?).to be true
     end
     it "returns false when created_at is not set" do
-      user = CloudConvert::Task.new()
-      expect(user.created?).to be false
+      task = CloudConvert::Task.new()
+      expect(task.created?).to be false
     end
   end
 
   describe "#started_at" do
     it "returns a Time when started_at is set" do
-      user = CloudConvert::Task.new(started_at: "Mon Jul 16 12:59:01 +0000 2020")
-      expect(user.started_at).to be_a Time
-      expect(user.started_at).to be_utc
+      task = CloudConvert::Task.new(started_at: "Mon Jul 16 12:59:01 +0000 2020")
+      expect(task.started_at).to be_a Time
+      expect(task.started_at).to be_utc
     end
     it "returns nil when started_at is not set" do
-      user = CloudConvert::Task.new()
-      expect(user.started_at).to be_nil
+      task = CloudConvert::Task.new()
+      expect(task.started_at).to be_nil
     end
   end
 
   describe "#started?" do
     it "returns true when started_at is set" do
-      user = CloudConvert::Task.new(started_at: "Mon Jul 16 12:59:01 +0000 2020")
-      expect(user.started?).to be true
+      task = CloudConvert::Task.new(started_at: "Mon Jul 16 12:59:01 +0000 2020")
+      expect(task.started?).to be true
     end
     it "returns false when started_at is not set" do
-      user = CloudConvert::Task.new()
-      expect(user.started?).to be false
+      task = CloudConvert::Task.new()
+      expect(task.started?).to be false
     end
   end
 
   describe "#ended_at" do
     it "returns a Time when ended_at is set" do
-      user = CloudConvert::Task.new(ended_at: "Mon Jul 16 12:59:01 +0000 2020")
-      expect(user.ended_at).to be_a Time
-      expect(user.ended_at).to be_utc
+      task = CloudConvert::Task.new(ended_at: "Mon Jul 16 12:59:01 +0000 2020")
+      expect(task.ended_at).to be_a Time
+      expect(task.ended_at).to be_utc
     end
     it "returns nil when ended_at is not set" do
-      user = CloudConvert::Task.new()
-      expect(user.ended_at).to be_nil
+      task = CloudConvert::Task.new()
+      expect(task.ended_at).to be_nil
     end
   end
 
   describe "#ended?" do
     it "returns true when ended_at is set" do
-      user = CloudConvert::Task.new(ended_at: "Mon Jul 16 12:59:01 +0000 2020")
-      expect(user.ended?).to be true
+      task = CloudConvert::Task.new(ended_at: "Mon Jul 16 12:59:01 +0000 2020")
+      expect(task.ended?).to be true
     end
     it "returns false when ended_at is not set" do
-      user = CloudConvert::Task.new()
-      expect(user.ended?).to be false
+      task = CloudConvert::Task.new()
+      expect(task.ended?).to be false
     end
   end
 end
