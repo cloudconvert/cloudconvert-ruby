1302a7792111d6db75592a6d0f5362e678306d1e
Josias Montag <josias@montag.info>
2021-04-21 11:38:47 +0200

    Replace mimemagic dependency

--- lib/cloudconvert/file.rb
+++ lib/cloudconvert/file.rb
@@ -2,7 +2,7 @@ module CloudConvert
   class File < Faraday::FilePart
     def initialize(file, content_type = nil, *parts)
       content_type ||= "text/plain" if file.is_a? StringIO
-      content_type ||= MimeMagic.by_magic(file) || MimeMagic.by_path(file)
+      content_type ||= Marcel::Magic.by_magic(file) || Marcel::Magic.by_path(file)
       super(file, content_type, *parts)
     end
   end
