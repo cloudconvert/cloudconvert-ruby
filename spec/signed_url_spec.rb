9e34ccb282a071ca7ca7d8829b757db2893b9577
Josias Montag <josias@montag.info>
2022-03-30 13:32:40 +0200

    fix code formatting

--- spec/signed_url_spec.rb
+++ spec/signed_url_spec.rb
@@ -1,12 +1,6 @@
 describe CloudConvert::SignedUrl, :unit do
-  payload = fixture("requests/webhook_job_finished_payload.json").read
-  signature = "576b653f726c85265a389532988f483b5c7d7d5f40cede5f5ddf9c3f02934f35"
-  invalid_signature = SecureRandom.alphanumeric(64)
-  secret = "secret"
-
   describe ".sign" do
     it "should return a signed url" do
-
       job = {
         tasks: {
           "import-it": { operation: "import/url", filename: "test.file", url: "http://invalid.url" },
@@ -26,8 +20,6 @@ describe CloudConvert::SignedUrl, :unit do
       expect(url).to include "?job="
       expect(url).to include "&cache_key=mykey"
       expect(url).to include "&s=3fb529168264bccea28ba9a1d02f2a4662d1917029829ee77e753a7748b98904"
-
     end
-
   end
 end
